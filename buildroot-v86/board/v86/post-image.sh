38205e313d59560b083517298d9b25cd7974e1ed
Cemal Gönültaş <45357531+cemalgnlts@users.noreply.github.com>
2023-06-01 12:37:50 +0300

    Update post-image.sh

--- buildroot-v86/board/v86/post-image.sh
+++ buildroot-v86/board/v86/post-image.sh
@@ -3,6 +3,7 @@
 # Run after buildroot has built the image, and path to the built
 # output/image dir is passed as first arg.  We copy the built ISO
 # out of the container.
-cp ${BINARIES_DIR}/rootfs.iso9660 /build/linux.iso
+mkdir -p ../dist/
+mv ${BINARIES_DIR}/rootfs.iso9660 ../dist/linux.iso
 
 echo "Created linux.iso."
