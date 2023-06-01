91859e589cce87ea4243d748f85be050cf972c1d
Cemal Gönültaş <45357531+cemalgnlts@users.noreply.github.com>
2023-06-01 12:56:36 +0300

    Path names customized

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ NODEJS_VERSION='v16.20.0'
 BUILD_ROOT_RELEASE='2023.02'
 
 # Create nodejs library directory if not exists
-NODE_LIB_DIR='buildroot-v86/board/v86/rootfs_overlay/usr/local/lib/nodejs'
+NODE_LIB_DIR='buildroot-config/board/rootfs_overlay/usr/local/lib/nodejs'
 if [ ! -d $NODE_LIB_DIR ]
 then
-    echo "Nodejs downloading..."
+    echo "[Nodejs] downloading..."
     mkdir -p $NODE_LIB_DIR
     cd $NODE_LIB_DIR
 
@@ -22,25 +22,29 @@ then
 
     cd -
 
-    echo "Nodejs downloaded."
+    echo "[Nodejs] downloaded."
+else
+    echo "[Nodejs] already exists."
 fi
 
 if [ ! -d "./buildroot-$BUILD_ROOT_RELEASE" ]
 then
-    echo "Buildroot downloading..."
+    echo "[Buildroot] downloading..."
 
     wget -q "http://buildroot.org/downloads/buildroot-$BUILD_ROOT_RELEASE.tar.gz"
     tar -axf "buildroot-$BUILD_ROOT_RELEASE.tar.gz"
 
-    echo "Buildroot downloaded"
+    echo "[Buildroot] downloaded"
+else
+    echo "[Buildroot] already exists."
 fi
 
-echo "./buildroot-v86 copying to ./buildroot-$BUILD_ROOT_RELEASE"
+echo "./buildroot-config copying to ./buildroot-$BUILD_ROOT_RELEASE"
 
-cp -fr ./buildroot-v86 ./buildroot-$BUILD_ROOT_RELEASE/
+cp -fr ./buildroot-config ./buildroot-$BUILD_ROOT_RELEASE/
 cd buildroot-$BUILD_ROOT_RELEASE
 
-# Build our v86 defconfig.
-make BR2_EXTERNAL=buildroot-v86 v86_defconfig && make
+# Build our defconfig.
+make BR2_EXTERNAL=buildroot-config defconfig && make
 
 echo "See ./dist for built ISO"
