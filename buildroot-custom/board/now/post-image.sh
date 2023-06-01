3a9fdb90ef30228eb2825d628bdd75d6a88f0bb3
Cemal Gönültaş <45357531+cemalgnlts@users.noreply.github.com>
2023-06-01 23:26:24 +0300

    Update post-image.sh

--- buildroot-custom/board/now/post-image.sh
+++ buildroot-custom/board/now/post-image.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Run after buildroot has built the image.
-mkdir -p ../dist/
-mv ${BINARIES_DIR}/rootfs.iso9660 ../dist/linux.iso
+pwd
+mkdir -p dist/
+mv ${BINARIES_DIR}/rootfs.iso9660 dist/linux.iso
 
 echo "Created linux.iso."
