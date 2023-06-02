50b87a62c945d7bca9aac248105f5c6f33bfd60b
Cemal Gönültaş <45357531+cemalgnlts@users.noreply.github.com>
2023-06-02 12:18:13 +0300

    Update old paths.

--- build.sh
+++ build.sh
@@ -4,47 +4,46 @@ NODEJS_VERSION='v16.20.0'
 BUILD_ROOT_RELEASE='2023.02'
 
 # Create nodejs library directory if not exists
-NODE_LIB_DIR='buildroot-now/board/rootfs_overlay/usr/local/lib/nodejs'
+NODE_LIB_DIR='buildroot-custom/board/now/rootfs_overlay/usr/local/lib/nodejs'
 if [ ! -d $NODE_LIB_DIR ]
 then
-    echo "[Nodejs] downloading..."
-    mkdir -p $NODE_LIB_DIR
-    cd $NODE_LIB_DIR
+    echo '[Nodejs] downloading...'
+    mkdir -p "$NODE_LIB_DIR"
+    cd "$NODE_LIB_DIR"
 
     # Download nodejs
-    VERSION="v16.20.0"
-    wget -q "https://unofficial-builds.nodejs.org/download/release/v16.20.0/node-$VERSION-linux-x86.tar.xz"
+    wget -q "https://unofficial-builds.nodejs.org/download/release/v16.20.0/node-$NODEJS_VERSION-linux-x86.tar.xz"
 
     # Unzip the binary archive
-    tar -axf "node-$VERSION-linux-x86.tar.xz"
-    mv node-$VERSION-linux-x86/{bin,include,lib,share} .
-    rm -rf "node-$VERSION-linux-x86" "node-$VERSION-linux-x86.tar.xz"
+    tar -axf "node-$NODEJS_VERSION-linux-x86.tar.xz"
+    mv node-$NODEJS_VERSION-linux-x86/{bin,include,lib,share} .
+    rm -rf "node-$NODEJS_VERSION-linux-x86" "node-$NODEJS_VERSION-linux-x86.tar.xz"
 
     cd -
 
-    echo "[Nodejs] downloaded."
+    echo '[Nodejs] downloaded.'
 else
-    echo "[Nodejs] already exists."
+    echo '[Nodejs] already exists.'
 fi
 
 if [ ! -d "./buildroot-$BUILD_ROOT_RELEASE" ]
 then
-    echo "[Buildroot] downloading..."
+    echo '[Buildroot] downloading...'
 
     wget -q "http://buildroot.org/downloads/buildroot-$BUILD_ROOT_RELEASE.tar.gz"
     tar -axf "buildroot-$BUILD_ROOT_RELEASE.tar.gz"
 
-    echo "[Buildroot] downloaded"
+    echo '[Buildroot] downloaded.'
 else
-    echo "[Buildroot] already exists."
+    echo '[Buildroot] already exists.'
 fi
 
 echo "./buildroot-custom copying to ./buildroot-$BUILD_ROOT_RELEASE"
 
-cp -fr ./buildroot-custom ./buildroot-$BUILD_ROOT_RELEASE/
-cd buildroot-$BUILD_ROOT_RELEASE
+cp -fr 'buildroot-custom/' "buildroot-$BUILD_ROOT_RELEASE/"
+cd "buildroot-$BUILD_ROOT_RELEASE"
 
 # Build our defconfig.
-sudo make BR2_EXTERNAL=buildroot-custom now_defconfig && make
+make BR2_EXTERNAL=buildroot-custom now_defconfig && make
 
 echo "See ./dist for built ISO"
