03928f258f1bf2e197d1ac1c16bd1f4b34a85cee
Cemal Gönültaş <45357531+cemalgnlts@users.noreply.github.com>
2023-06-03 11:26:07 +0300

    More customization improvements.

--- buildroot-custom/board/now/rootfs_overlay/opt/now/boot-node.sh
+++ buildroot-custom/board/now/rootfs_overlay/opt/now/boot-node.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 ITERATION=10
-print "[1/$ITERATION] Node initializing..."
+printf "[1/$ITERATION] Node initializing..."
 
 for i in `seq $ITERATION`
 do
     node /opt/now/hello.js > /dev/null
 
-    print "\r[$i/$ITERATION] Node initializing..."
+    printf "\r[$i/$ITERATION] Node initializing..."
 done
 
 echo
