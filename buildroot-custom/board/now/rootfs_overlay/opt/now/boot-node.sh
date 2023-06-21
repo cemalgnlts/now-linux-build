4be83cc71c143c31ec18f3ddfe4e8324c71a89cd
Cemal Gönültaş <45357531+cemalgnlts@users.noreply.github.com>
2023-06-21 11:23:21 +0300

    Do not remove system files.

--- buildroot-custom/board/now/rootfs_overlay/opt/now/boot-node.sh
+++ buildroot-custom/board/now/rootfs_overlay/opt/now/boot-node.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-umount /proc
-umount /sys
-umount /run
-
-rm -rf /media /etc /proc /mnt /sys /run /var
-rm -r /lib32 /init
-
 ITERATION=10
 echo "Node initializing..."
 
@@ -14,5 +7,3 @@ for i in `seq $ITERATION`
 do
     time -f "[$i/$ITERATION] - %es" node -e 'console.log("Hello World")' > /dev/null
 done
-
-rm -rf /dev
